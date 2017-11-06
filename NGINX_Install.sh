be0d462ee46bda31175e0cf97c172c042d4bf372
ngfellow <33337520+ngfellow@users.noreply.github.com>
2017-11-05 21:20:19 -0700

    fixed ports to open

--- NGINX_Install.sh
+++ NGINX_Install.sh
@@ -5,9 +5,10 @@ apt-get update
 apt-get install nginx -y
 # Turning Firewall On
 ufw enable
-# Open Ports 3200, 80, and 22
-ufw allow 3200/tcp
-ufw allow 22/tcp
-ufw allow 80/tcp
+# Open Ports 3200, 3400, 80, and 22
+ufw allow 3400
+ufw allow 3200
+ufw allow 22
+ufw allow 80
 # Confirm NGINX to start on boot
 update-rc.d nginx defaults
